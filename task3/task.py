de54530d06e6a1ce84d6042cebba3160c8037204
Nina Samykova <118392730+Nnisik@users.noreply.github.com>
2024-01-23 15:07:15 +0300

    Fixed task.py

--- task3/task.py
+++ task3/task.py
@@ -1,29 +1,64 @@
 import csv
 import math
 
-def calculate_entropy(n: int, k: int, l):
-    H = 0
-    for i in range(1, n+1):
-        for j in range(1, k+1):
-            H += (l/(n - k)) * math.log10(l/(n * k))
-    return H * (-1)
 
-def task(csv_path: str) -> None:
-    entropy = 0
+def _read_relation_matrix_from_csv(filepath):
+    matrix = []
+    with open(filepath, "r") as table:
+        csvreader = csv.reader(table)
+        for row in csvreader:
+            matrix.append(row)
 
-    # read csv file
-    with open(csv_path, "r") as csv_file:
-        csv_reader = csv.reader(csv_file)
-        data = list(csv_reader)
-        len_data = len(data)
-    
-    for row in data:
-        for elem in row:
-            entropy += calculate_entropy(len(row), len_data, elem) 
-    
+    return matrix
+
+
+def _compute_probability_matrix(matrix):
+    prob_matrix = []
+    n = len(matrix)
+    for row in matrix:
+        temp_probs = []
+        for value in row:
+            temp_probs.append(float(value) / (n - 1))
+
+        prob_matrix.append(temp_probs)
+
+    return prob_matrix
+
+
+def _compute_entropy_for_row(prob_matrix):
+    entropies = []
+
+    for row in prob_matrix:
+        h = 0.0
+        for value in row:
+            if value != 0.0:
+                h += -1 * value * math.log2(value)
+
+        entropies.append(h)
+
+    return entropies
+
+
+def task(matrix):
+    prob_matrix = _compute_probability_matrix(matrix)
+    entropies = _compute_entropy_for_row(prob_matrix)
+
+    entropy = 0.0
+    for value in entropies:
+        entropy += value
+
     return entropy
 
 
-if __name__ == "__main__": 
-    file_path = "./task3.csv"
-    print(task(file_path))
+if __name__ == "__main__":
+    matrix = [
+        [1, 0, 2, 0, 0],
+        [2, 1, 2, 0, 0],
+        [2, 1, 0, 1, 1],
+        [0, 1, 0, 1, 1],
+        [0, 1, 0, 1, 1],
+        [0, 1, 0, 1, 1],
+    ]
+
+    res = task(matrix)
+    print(res)
